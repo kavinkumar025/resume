1a52d79be6fc3d8142e8d121a78c08c42d5ae06a
kavinkumar025 <kavinkumarkk026@gmail.com>
2023-07-20 15:44:22 +0530

    typing effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume';
+  @ViewChild('textContainer', { static: true })
+  textContainerRef!: ElementRef;
+  roles = ['Angular Developer', 'Front-end web Developer', 'Data Modeler'];
+  index = 0;
+  roleIndex = 0;
+  isInDeletingState = false;
+
+  ngOnInit() {
+    this.typeWriter();
+  }
+
+  public typeWriter() {
+    const textContainer: HTMLElement = this.textContainerRef.nativeElement;
+    const currentRole = this.roles[this.roleIndex];
+    const currentText = currentRole.slice(0, this.index);
+    textContainer.textContent = currentText;
+    if (!this.isInDeletingState) {
+      this.index++;
+      if (this.index > currentRole.length) {
+        this.isInDeletingState = true;
+        setTimeout(() => this.typeWriter(), 2000); // Pause before deleting
+      } else {
+        setTimeout(() => this.typeWriter(), 100); // Typing speed
+      }
+    } else {
+      this.index--;
+      if (this.index === 0) {
+        this.isInDeletingState = false;
+        this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+      }
+      setTimeout(() => this.typeWriter(), 50); // Deleting speed
+    }
+  }
 }
