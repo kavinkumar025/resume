669edc7ec34d181a4d4c2fd14645057f6abbfc8f
AzureAD\KavinKumar <kavin.kumar@franklin-madison.com>
2024-08-14 18:22:11 +0530

    'Updated-Code'

--- src/app/profile/header/header.component.ts
+++ src/app/profile/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+  activeSection: string = 'introduction'; // Default section
+
+  sections = [
+    { id: 'about', offset: 0 },
+    { id: 'experience', offset: 0 },
+    { id: 'skill', offset: 0 },
+    { id: 'education', offset: 0 },
+    { id: 'contact', offset: 0 }
+  ];
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.sections.forEach(section => {
+      const element = document.getElementById(section.id);
+      if (element) {
+        section.offset = element.offsetTop;
+      }
+    });
+
+    const scrollPosition = window.pageYOffset + window.innerHeight / 2;
+
+    for (let section of this.sections) {
+      if (scrollPosition >= section.offset) {
+        this.activeSection = section.id;
+      }
+    }
+  }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+
+  public sections = [
+    { id: 'introduction', title: 'Introduction' },
+    { id: 'about', title: 'About Me' },
+    { id: 'experience', title: 'Experience' },
+    { id: 'skill', title: 'Skills' },
+    { id: 'education', title: 'Education' },
+    { id: 'contact', title: 'Contact' }
+  ];
+
+  constructor(private router: Router, private titleService: Title) { }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let currentSectionId = '';
+    for (let section of this.sections) {
+      const element = document.getElementById(section.id);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        if (rect.top >= 0 && rect.top <= window.innerHeight / 2) {
+          currentSectionId = section.id;
+          this.titleService.setTitle(section.title);
+          this.router.navigate([], { fragment: currentSectionId });
+          break;
+        }
+      }
+    }
+  }
 
   public onRightClick(event: MouseEvent): void {
     event.preventDefault();
