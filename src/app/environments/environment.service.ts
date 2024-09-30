d13835e495468ef78f6dbe350ffda4a6c5bf1e4b
KAVINKUMAR\Hi <kavinkumar025@gmail.com>
2024-09-30 10:43:03 +0530

    Revert "Added new Date parameter"
    This reverts commit 9c0463a0902a19b5b9aa79c443fe370568422db8.

--- src/app/environments/environment.service.ts
+++ src/app/environments/environment.service.ts
@@ -7,10 +7,20 @@ import { getAnalytics } from 'firebase/analytics';
 })
 
 export class EnvironmentService {
-  static portfolio(value: string, portfolio: any) {
-    throw new Error('KavinKumar');
-  }
 
-  public portfolio = 'KavinKumar';
+  public app: FirebaseApp;
+  public firebaseConfig = {
+    apiKey: "AIzaSyDDFRIxoujk_rAcWEM2DAzlDJu0iZE70z4",
+    authDomain: "my-prortfolio-e2f51.firebaseapp.com",
+    projectId: "my-prortfolio-e2f51",
+    storageBucket: "my-prortfolio-e2f51.appspot.com",
+    messagingSenderId: "119998992581",
+    appId: "1:119998992581:web:0ff8cd31ff6dc9288d60ac",
+    measurementId: "G-1M4V5XZHL3"
+  };
+
+  constructor() {
+    this.app = initializeApp(this.firebaseConfig);
+  }
 
 }
--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -25,20 +25,12 @@ export class ContactComponent {
   public submitForm() {
     if (this.contactForm.valid) {
       const formValue = this.contactForm.value;
-      const currentDate = new Date();
-      const day = String(currentDate.getDate()).padStart(2, '0');
-      const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-      const year = currentDate.getFullYear();
-      const hours = String(currentDate.getHours()).padStart(2, '0');
-      const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-      const istFormattedDate = `${day}-${month}-${year} ${hours}:${minutes}`;
       const dataToStore = {
         name: formValue.name,
         subject: formValue.subject,
         email: formValue.email,
         phone: Number(formValue.phone),
         description: formValue.description,
-        dateTime: istFormattedDate
       };
       this.firestore.collection('contacts').add(dataToStore)
         .then(() => {
@@ -55,15 +47,9 @@ export class ContactComponent {
         .catch((error) => {
           console.error('Error saving data: ', error);
         });
-    }
-    else {
-      Swal.fire({
-        position: "center",
-        icon: "error",
-        title: "Invalid form",
-        showConfirmButton: true
-      });
+    } else {
       console.log('Invalid form');
     }
   }
+
 }
